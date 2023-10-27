4cf79eaefcf439a9be629b7972e050ed5438b11d
DESKTOP-SLIEA8S\windows <1200967@isep.ipp.pt>
2023-10-27 15:34:12 +0100

    [US_170] Alteracao

--- src/repos/edificioRepo.ts
+++ src/repos/edificioRepo.ts
@@ -32,15 +32,16 @@ export default class EdificioRepo implements IEdificioRepo {
 
   public async save(edificio: Edificio): Promise<Edificio> {
     const query = { domainId: edificio.id.toString() };
-
+    
     const edificioDocument = await this.edificioSchema.findOne(query);
-
+    
     try {
       if (edificioDocument === null) {
         
-        const rawEdificio: any = EdificioMap.toPersistence(edificio);
+        const rawEdificio: any = EdificioMap.toPersistence(edificio);       
         
         const edificioCreated = await this.edificioSchema.create(rawEdificio);
+        
         
         return EdificioMap.toDomain(edificioCreated);
    
@@ -68,13 +69,18 @@ export default class EdificioRepo implements IEdificioRepo {
   }
 
   public async findAll(): Promise<Edificio[]> {
-    const edificioRecord = await this.edificioSchema.find().populate('dimensaoMaximaPisos');
+    
+    const edificioRecord = await this.edificioSchema.find();
+    
     if (edificioRecord != null) {
-      const edificioPromises = edificioRecord.map(async postRecord => {
+      const edificioPromises = edificioRecord.map(
+        async postRecord => {
+        
         return await EdificioMap.toDomain(postRecord);
       });
+      
       const edificios = await Promise.all(edificioPromises);
-     
+      console.log(edificios.at(0));
       return edificios;
     } else {
       return null;
