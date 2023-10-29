2dbe4a9e1fe525292adbca49ad9e8f0b7e49a32f
DESKTOP-SLIEA8S\windows <1200967@isep.ipp.pt>
2023-10-29 21:54:36 +0000

    [US_150][TESTS] Testes DescricaoEdificio

---
+++ tests/unit/services/edificio/descricaoEdificio.test.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import { DescricaoEdificio } from '../../../../src/domain/edificio/descricaoEdificio';
+
+describe('DescricaoEdificio Unit Tests', () => {
+  it('create valid descricaoEdificio', () => {
+    const value = 'descricao';
+    const descricao = DescricaoEdificio.create(value);
+    expect(descricao.isSuccess).to.equal(true);
+  });
+
+  /*it('fail to create descricaoEdificio with empty string', () => {
+    const value = '';
+    const descricao = DescricaoEdificio.create(value);
+    expect(descricao.isFailure).to.equal(true);
+  });*/
+
+  it('fail to create descricaoEdificio with null string', () => {
+    const value = null;
+    const descricao = DescricaoEdificio.create(value);
+    expect(descricao.isFailure).to.equal(true);
+  });
+
+  it('fail to create descricaoEdificio with undefined string', () => {
+    const value = undefined;
+    const descricao = DescricaoEdificio.create(value);
+    expect(descricao.isFailure).to.equal(true);
+  });
+});
