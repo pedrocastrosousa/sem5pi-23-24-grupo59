6596a58af4cbbe012ec0bdbe2bcb996ee26159a4
pedrocastrosousa <1201326@isep.ipp.pt>
2023-11-05 18:19:37 +0000

    [US370] fix Robot ID in save

--- src/services/IRepos/IRobotRepo.ts
+++ src/services/IRepos/IRobotRepo.ts
@@ -1,7 +1,7 @@
 import { Repo } from "../../core/infra/Repo";
 import { Robot } from "../../domain/robot/robot";
 
-export default interface IelevadorRepo extends Repo<Robot> {
+export default interface IRobotRepo extends Repo<Robot> {
 	save(robot: Robot): Promise<Robot>;
 	findById (id: string): Promise<Robot>;
 	findAll() : Promise<Array<Robot>>;
