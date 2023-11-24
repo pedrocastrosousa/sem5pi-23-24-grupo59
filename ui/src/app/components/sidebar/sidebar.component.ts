e40c90937d15fca3e40b01838cc2e60d13668de0
DESKTOP-SLIEA8S\windows <1200967@isep.ipp.pt>
2023-11-24 12:28:06 +0000

    [UI] Alterar opcoes da sidebar

--- ui/src/app/components/sidebar/sidebar.component.ts
+++ ui/src/app/components/sidebar/sidebar.component.ts
@@ -1,7 +1,5 @@
 /* eslint-disable prettier/prettier */
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Router } from '@angular/router';
-import { SidebarItem } from '../../domain/sidebarItem';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,19 +7,6 @@ import { SidebarItem } from '../../domain/sidebarItem';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-  @Output() itemSelected = new EventEmitter<string>();
-
-  sidebarItems: SidebarItem[] = [
-    { name: 'Edificio', subitems: ['criar-edificio', 'editar-edificio', 'listar-edificios'] },
-    { name: 'X', subitems: ['Subitem 2.1', 'Subitem 2.2', 'Subitem 2.3'] },
-    // Add more items with or without subitems
-  ];
-
-  constructor(private router: Router) { }
-
-  selectSubitem(event: any, item: SidebarItem) {
-    const selectedValue = event.target.value;
-    const selectedItem = `${item.name} - ${selectedValue}`;
-    this.itemSelected.emit(selectedItem);
-  }
+  
 }
+
